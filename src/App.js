e910931bda887405e69ab4e10dd563f9ce3b8879
Aidil <aidilrozi83@gmail.com>
2021-04-21 15:05:17 -0500

    Created states for inputs. Will start working on the back-end.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import "./App.css";
+import styled from "styled-components/macro";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const handleClick = (e) => {
+    console.log(name, age, country, position, wage);
+    e.preventDefault();
+  };
+
+  return (
+    <Container>
+      <label>Name:</label>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <label>Age:</label>
+      <input type="number" onChange={(e) => setAge(e.target.value)} />
+      <label>Country:</label>
+      <input type="text" onChange={(e) => setCountry(e.target.value)} />
+      <label>Position:</label>
+      <input type="text" onChange={(e) => setPosition(e.target.value)} />
+      <label>Wage (year)</label>
+      <input type="number" onChange={(e) => setWage(e.target.value)} />
+      <button onClick={handleClick}>Add Employee</button>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  label {
+    margin-bottom: 0.5em;
+  }
+
+  input {
+    height: 50px;
+    width: 300px;
+    font-size: 1.2rem;
+  }
+  button {
+    margin-top: 2em;
+    background-color: #ff0;
+    padding: 1em;
+    font-weight: bold;
+    &:hover {
+      background-color: orange;
+    }
+  }
+`;
 
 export default App;
