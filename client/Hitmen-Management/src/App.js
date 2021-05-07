a47b7b2ddd83868741bfd7259987675ea5587cd0
Aidil83 <aidilrozi83@gmail.com>
2021-05-07 18:57:28 -0500

    Finished with the create and read functionalities. Working on the delete functionality.

--- client/Hitmen-Management/src/App.js
+++ client/Hitmen-Management/src/App.js
@@ -2,43 +2,86 @@ import "./App.css";
 import styled from "styled-components/macro";
 import { useState } from "react";
 import Axios from "axios";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [country, setCountry] = useState("");
-  const [position, setPosition] = useState("");
-  const [wage, setWage] = useState(0);
+  const [location, setLocation] = useState("");
+  const [hitmanList, setHitmanList] = useState([])
 
-  const addEmployee = () => {
-    console.log(name, age, country, position, wage);
+
+  // Create endpoint
+  const addCustomer = () => {
+    // console.log(name, age, location, position, wage);
+    console.log(name, location);
 
     Axios.post("http://localhost:3001/create", {
       // Passing values to the back-end.
       name: name,
-      age: age,
-      country: country,
-      position: position,
-      wage: wage,
+      location: location,
     }).then(() => {
       console.log("success");
     }); // This is the endpoint that we want to make request.
   };
 
+  // Delete endpoint
+  const deleteHitman = (id) => {
+    axios.delete(`http://localhost:3001/delete/${id}`)
+  }
+
+
+  const getHitman = () => {
+    Axios.get("http://localhost:3001/hitman").then((res) => {
+      console.log(res);
+    })
+  }
+  const showHitman = () => {
+    Axios.get("http://localhost:3001/hitman").then((res) => {
+        setHitmanList(res.data);
+    })
+  }
+
   return (
-    <Container>
-      <label>Name:</label>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
-      <label>Age:</label>
-      <input type="number" onChange={(e) => setAge(e.target.value)} />
-      <label>Country:</label>
-      <input type="text" onChange={(e) => setCountry(e.target.value)} />
-      <label>Position:</label>
-      <input type="text" onChange={(e) => setPosition(e.target.value)} />
-      <label>Wage (year)</label>
-      <input type="number" onChange={(e) => setWage(e.target.value)} />
-      <button onClick={addEmployee}>Add Employee</button>
-    </Container>
+    <Router>
+      <Switch>
+          <Route path="/" exact>
+            <Container>
+              <label>Name:</label>
+              <input type="text" onChange={(e) => setName(e.target.value)} />
+              <label>location:</label>
+              <input type="text" onChange={(e) => setLocation(e.target.value)} />
+              <button onClick={addCustomer}>Add customer</button>
+              <Link to="/page2">
+                <button>Next</button>
+              </Link>
+            </Container>
+          </Route>
+          <Route path="/page2" exact>
+            <Container>
+              <button onClick={getHitman}>Get Hitmen</button>
+              <button onClick={showHitman}>Show Hitmen</button>
+              <Link to="/">
+                <button>Go Back</button>
+              </Link>
+              {hitmanList.map((val) => {
+                return <div className="hitman-container" key={val.h_id}>
+                  <div><b>name:</b> {val.name}</div>
+                  <div><b>location:</b> {val.location}</div>
+                  <div><b>ranking:</b> {val.ranking}</div>
+                  <div><b>cost:</b> {val.cost}</div>
+                  <div><b>specialties:</b> {val.specialties}</div>
+                  <button onClick={() => {deleteHitman(val.h_id)}}>Delete</button>
+                </div>
+              })}
+            </Container>
+          </Route>
+      </Switch>
+    </Router>
   );
 }
 
@@ -66,6 +109,23 @@ const Container = styled.div`
     font-weight: bold;
     &:hover {
       background-color: orange;
+      cursor: pointer;
+    }
+  }
+  & .hitman-container {
+    width: 800px;
+    height: min-content;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1em;
+    margin-top: 1em;
+    border: 1px solid black;
+    background-color: lightgray;
+    button {
+      align-self: center;
+      margin-top: 0;
+
     }
   }
 `;
